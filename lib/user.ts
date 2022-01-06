cb612a40b0282482b06d78e490e8c0e65e8462b1
p-acid <qkrtkstjd01@gmail.com>
2022-01-06 16:58:08 +0900

    modify: conflict

--- lib/user.ts
+++ lib/user.ts
@@ -3,6 +3,17 @@ import axios from 'axios';
 
 // about user function
 
+export function getUserData(token: string) {
+  const res = fetch(`${URI}/v1/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(res => res.json());
+
+  return res;
+}
+
 export const submitUserData = (userData: object) => {
   const res = axios.post(`${URI}/v1/auth/local`, userData, {
     headers: {
