1d92aaa835ab4c865b17f3ff2ff05aacb0ff8443
p-acid <qkrtkstjd01@gmail.com>
2022-01-11 18:38:33 +0900

    Modify: URL & PATH Naming

--- api/main.ts
+++ api/main.ts
@@ -1,23 +1,22 @@
-import { AxiosResponse } from 'axios';
 import { CookieValueTypes } from 'cookies-next/lib/types';
 
 import req from './core';
-import { URL } from './core';
+import { PATH } from './core';
 
 export const getUserData = (token: CookieValueTypes) => {
-  const res = req.get(URL.myInfo, { headers: { Authorization: `Bearer ${token}` } });
+  const res = req.get(PATH.myInfo, { headers: { Authorization: `Bearer ${token}` } });
 
   return res;
 };
 
 export const getListData = async (params: object) => {
-  const res = req.get(URL.contentsList, { params: params });
+  const res = req.get(PATH.contentsList, { params: params });
 
   return res;
 };
 
 export const submitUserData = async (userData: object) => {
-  const res = req.post(URL.signIn, userData, {
+  const res = req.post(PATH.signIn, userData, {
     headers: {
       'content-type': 'application/json',
     },
